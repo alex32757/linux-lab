c5ac7b54d3d8247fadf45c2db5cbc8b915061b1c
Alexandr <alex32757@gmail.com>
2021-10-21 13:04:10 +0300

    add 7 lab

--- child2.cpp
+++ child2.cpp
@@ -7,11 +7,10 @@
 #include <wait.h>
 
 using namespace std;
-
 bool flag = true;
 
 void signal_handler(int signum) {
-	cout << " Quit1" << endl;
+	cout << " Quit2" << endl;
 	flag = false;
 }
 
@@ -19,24 +18,26 @@ int main(int argc, char** argv) {
 	ofstream out(argv[1]);
 	int signum, bytes;
 	char c;
-
-	cout << "P1 start" << endl;
+	
+	cout << "P2 start" << endl;
 	sigset_t mask;
 	sigemptyset(&mask);
-	sigaddset(&mask, SIGUSR1);
+	sigaddset(&mask, SIGUSR2);
 	sigprocmask(SIG_BLOCK, &mask, nullptr);
-	signal(SIGINT, signal_handler);
 
+	signal(SIGINT, signal_handler);
+	
+	sigwait(&mask, &signum);
 	while((bytes = read(*argv[2], &c, 1)) || flag) {
 		// if (bytes == 0)
 		// 	break;
-		cout << "P1 ch: " << c << endl;
+		cout << "P2 ch: " << c << endl;
 		out << c;
-		kill(0, SIGUSR2);
-		sigwait(&mask, &signum);
+		kill(0, SIGUSR1);
+		sigwait(&mask, &signum);		
 	}
-	kill(0, SIGUSR2);
-
+	kill(0, SIGUSR1);
 	out.close();
 	exit(EXIT_SUCCESS);
+	return 0;
 }
--- lab7.cpp
+++ lab7.cpp
@@ -1,65 +1,62 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <unistd.h>
+#include <cstring>
 #include <sys/wait.h>
-
-bool pid1_ready = false;
-bool pid2_ready = false;
-
-void handlerUsr1(int sig) {
-    pid1_ready = true;
-}
-
-void handlerUsr2(int sig) {
-    pid2_ready = true;
-}
+#include <signal.h>
+#include <wait.h>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    ifstream file(argv[1]);
-    int pipe1[2];
-    pipe(pipe1);
+	ifstream file1(argv[1]);
 
-    string pipe_read = to_string(pipe1[0]);
-    string psig1 = to_string(SIGUSR1);
-    string psig2 = to_string(SIGUSR2);
+	int pipe_d[2];
+	pid_t pid1, pid2;
+	string buf;
 
-    signal(SIGUSR1, handlerUsr1);
-    signal(SIGUSR2, handlerUsr2);
+	sigset_t new_mask;
+	sigaddset(&new_mask, SIGUSR1);
+	sigaddset(&new_mask, SIGUSR2);
+	sigaddset(&new_mask, SIGQUIT);
+	sigprocmask(SIG_SETMASK, &new_mask, NULL);
 
-    pid_t pid1 = fork();
-    if (pid1 == 0)
-        execl("./child", "./child", argv[2], (char*)pipe_read.data(), (char*)psig1.data(), NULL);
-    
-    pid_t pid2 = fork();
-    if (pid2 == 0)
-        execl("./child", "./child", argv[3], (char*)pipe_read.data(), (char*)psig2.data(), NULL);
+	pipe(pipe_d);
 
-    while (!(pid1_ready && pid2_ready));
-    while (!file.eof()) {
-        char c;
-        if ((c = file.get()) != EOF) { 
-            write(pipe1[1], &c, 1);
-            pid1_ready = false;
-            kill(pid1, SIGUSR1);
-            while (!(pid1_ready));
-        }
+	pid1 = fork();
+	if (pid1 == 0) {
+		// Child1 
+		close(pipe_d[1]);
+		execl("child1", "child1", argv[2], &pipe_d[0], NULL);
+	}
+	else {
+		pid2 = fork();
+		if (pid2 == 0) {
+			// Child2 
+			close(pipe_d[1]);
+			execl("child2", "child2", argv[3], &pipe_d[0], NULL);
+		}
+		else {
+			// Parent 
+			close(pipe_d[0]);
+			sleep(1);
+			while(getline(file1, buf)){
+				//sleep(1);
+				write(pipe_d[1], &buf[0], strlen(buf.c_str()));;
+				cout << "Parent write symbol:" <<  &buf[0] << endl;
+				
+			}
+			
+			kill(pid1, SIGINT);
+			kill(pid2, SIGINT);
+			cout << "Parent end writing" << endl;
+			file1.close();
+			
+			close(pipe_d[1]);
+			waitpid(pid1, nullptr, 0);
+			waitpid(pid2, nullptr, 0);
+		}
+	}
 
-        if ((c = file.get()) != EOF) {
-            write(pipe1[1], &c, 1);
-            pid2_ready = false;
-            kill(pid2, SIGUSR2);
-            while (!(pid2_ready));
-        }
-    }
-
-    kill(pid1, SIGQUIT);
-    kill(pid2, SIGQUIT);
-
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-
-    return 0;
+	return 0;
 }
